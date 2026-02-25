2438ceccb9d5b876f44d92063deda8a8586bac32
Giovanni-Hernandez10 <giovannihernandez10209@gmail.com>
2026-02-24 16:22:07 -0800

    creating basic gin server and running it with bazel

---
+++ BUILD
@@ -0,0 +1,4 @@
+load("@gazelle//:def.bzl", "gazelle")
+
+# gazelle:prefix github.com/Giovanni-Hernandez10/ZillaRCE
+gazelle(name = "gazelle")
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,3 +1,14 @@
-package api
+package main
 
 // Entry point for the HTTP API -> starts a gin server
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message: ": "pong"})
+	})
+	r.Run()
+}
