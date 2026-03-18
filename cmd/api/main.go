d25498e67ed04af75b759c0a7f74c3ff593bad65
Giovanni-Hernandez10 <giovannihernandez10209@gmail.com>
2026-03-18 15:08:11 -0700

    Testing creating a docker client from cmd/api/main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,23 @@ package main
 
 // Entry point for the HTTP API -> starts a gin server
 import (
+	"fmt"
+
+	"github.com/Giovanni-Hernandez10/ZillaRCE/internal/sandbox"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	dockerClient, err := sandbox.CreateDockerClient("python:alpine", "python")
+	if err != nil {
+		fmt.Print("Error setting up docker client")
+	}
+	if dockerClient != nil {
+		fmt.Printf("Created docker client")
+	}
+	fmt.Printf("The docker client was successfully created")
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message: ": "pong"})
 	})
--- internal/sandbox/docker.go
+++ internal/sandbox/docker.go
@@ -1,3 +1,32 @@
 package sandbox
 
 // docker orchestration logic -> spinning up containers, adding security, resource limits
+
+import (
+	// "context"
+
+	"github.com/moby/moby/client"
+)
+
+// Struct to hold the created docker client
+// has img and language associated with it
+type ClientSandbox struct {
+	cli      *client.Client
+	img      string
+	language string
+}
+
+// Method to create a new docker client
+// Pass in the img and language to create the docker client for
+func CreateDockerClient(img string, language string) (*ClientSandbox, error) {
+	cli, err := client.New(client.FromEnv)
+	if err != nil {
+		return nil, err
+	}
+	return &ClientSandbox{cli: cli, img: img, language: language}, nil
+}
+
+// // Method to run the container with the desired image and command
+// func (c *ClientSandbox) RunContainer(ctx context.Context, image string, cmd []string) {
+
+// }
